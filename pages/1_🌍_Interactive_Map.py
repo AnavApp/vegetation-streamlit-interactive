4934732632edbb8a405f821528613cde53a3ba28
AnavApp <118699849+AnavApp@users.noreply.github.com>
2024-03-29 15:21:29 -0700

    Fixed service account code

--- pages/1_🌍_Interactive_Map.py
+++ pages/1_🌍_Interactive_Map.py
@@ -23,9 +23,9 @@ json_data=st.secrets["json_data"]
 service_account = st.secrets["service_account"]
 
 
-# json_object = json.loads(json_data, strict=False)
+json_object = json.loads(json_data, strict=False)
 # service_account = json_object['client_email']
-# json_object = json.dumps(json_object)
+json_object = json.dumps(json_object)
 # Authorising the app
 credentials = ee.ServiceAccountCredentials(service_account, key_data=json_object)
 ee.Authenticate()
